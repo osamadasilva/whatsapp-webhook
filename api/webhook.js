fb5e356245f7d4794216fb3fcf2665f5e3d69aa4
osamadasilva <osamah19919@gmail.com>
2026-03-25 09:39:51 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -9,12 +9,12 @@ const mode = req.query[“hub.mode”];
 const token = req.query[“hub.verify_token”];
 const challenge = req.query[“hub.challenge”];
 
-```
+
 if (mode === "subscribe" && token === process.env.WHATSAPP_VERIFY_TOKEN) {
   return res.status(200).send(challenge);
 }
 return res.status(403).send("Forbidden");
-```
+
 
 }
 
